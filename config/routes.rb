de9cbb81259726a836c896f3d7f83b8645c20542
zeus-nishant <nishantpal1409@gmail.com>
2023-05-31 17:55:03 +0530

    2nd Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :blog_posts
+  
 
   # Defines the root path route ("/")
    root "blog_posts#index"
 end
+
